a28ef05e9396527824e0767c6f78a8671e0fa653
Baharudin78 <bencoxgame78@gmail.com>
2023-06-26 21:45:58 +0700

    beer screen

--- app/src/main/java/com/baharudin/pagingcaching/MainActivity.kt
+++ app/src/main/java/com/baharudin/pagingcaching/MainActivity.kt
@@ -10,8 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.baharudin.pagingcaching.presentation.BeerScreen
+import com.baharudin.pagingcaching.presentation.BeerViewModel
 import com.baharudin.pagingcaching.ui.theme.PagingCachingTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +28,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val viewModel = hiltViewModel<BeerViewModel>()
+                    val beer = viewModel.beerPagingFlow.collectAsLazyPagingItems()
+                   BeerScreen(beers = beer )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PagingCachingTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/baharudin/pagingcaching/presentation/BeerItems.kt
+++ app/src/main/java/com/baharudin/pagingcaching/presentation/BeerItems.kt
@@ -1,10 +1,18 @@
 package com.baharudin.pagingcaching.presentation
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.room.PrimaryKey
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.baharudin.pagingcaching.domain.Beer
 import com.baharudin.pagingcaching.ui.theme.PagingCachingTheme
 
@@ -13,7 +21,57 @@ fun BeerItems(
     beer : Beer,
     modifier: Modifier = Modifier
 ) {
-
+    Card(
+        modifier = modifier,
+        elevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Max)
+                .padding(16.dp)
+        ) {
+            AsyncImage(
+                model = beer.imageUrl ,
+                contentDescription = beer.name,
+                modifier = Modifier
+                    .weight(1f)
+                    .height(150.dp)
+            )
+            Spacer(modifier =Modifier.width(16.dp))
+            Column(
+                modifier = Modifier
+                    .weight(3f )
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = beer.name,
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = beer.tagline,
+                    fontStyle = FontStyle.Italic,
+                    color = Color.LightGray,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = beer.description,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "First Brewed in ${beer.firstBrewed}",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.End,
+                    fontSize = 8.sp
+                )
+            }
+        }
+    }
 }
 
 @Preview
